54ad4902c8ae553956b491a5726f16c1dc2d662d
RupeshSharan <147916199+RupeshSharan@users.noreply.github.com>
2024-07-07 14:01:31 +0530

    Update ROCK,PAPER,SCISSOR GAME.py

--- ROCK,PAPER,SCISSOR GAME.py
+++ ROCK,PAPER,SCISSOR GAME.py
@@ -30,17 +30,31 @@ def play_game(user_choice):
     '''
 
     tools = [rock, paper, scissors]
+    global player_score
+    global computer_score
 
     computer_choice = random.randint(0, 2)
 
     if (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
-        result = f"You chose: {tools[user_choice]} \nThe computer chose: {tools[computer_choice]}\n You win!"
+        player_score += 1
+        result = f"You chose: {tools[user_choice]} \nThe computer chose: {tools[computer_choice]}\n You win! your score {player_score}"
     elif user_choice == computer_choice:
         result = f"You chose: {tools[user_choice]} \nThe computer chose: {tools[computer_choice]}\n It's a draw!"
     else:
-        result = f"You chose {tools[user_choice]} \nThe computer chose {tools[computer_choice]}\n You lose!"
-
-    result_text.set(result)
+        computer_score += 1
+        result = f"You chose {tools[user_choice]} \nThe computer chose {tools[computer_choice]}\n You lose! computer score {computer_score}"
+    if (player_score >= 10) or (computer_score >= 10):
+       if player_score > computer_score:
+            Result = f"Player won the game with {player_score}\n Thank for playing the game"
+       else:
+           Result = f"Computer won the game with {computer_score}\n Thank for playing the game"
+       result_text.set(result)
+       Result_text.set(Result)
+       for widget in main_window.winfo_children():
+           widget.pack_forget()
+       tk.Label(main_window, text=Result, bg="lightpink", font="Lucida").pack(pady=10)
+    else:
+       result_text.set(result)
 
 main_window = tk.Tk()
 main_window.title("Rock, Paper, Scissors")
@@ -54,7 +68,14 @@ tk.Button(main_window,text="Paper",bg="skyblue",command=lambda:play_game(1),font
 tk.Button(main_window,text="Scissors",bg="red",command=lambda:play_game(2),font="Lucida").pack(pady=5)
 
 result_text = tk.StringVar()
-result_label = tk.Label(main_window, textvariable=result_text, font=('Arial', 12), wraplength=300)
+result_label = tk.Label(main_window, textvariable=result_text, font=('Arial', 12),bg="aqua", wraplength=300)
 result_label.pack(pady=10)
+
+Result_text = tk.StringVar()
+Result_label = tk.Label(main_window, textvariable=Result_text, font=('Arial', 12),bg="brown", wraplength=300)
+Result_label.pack(pady=10)
+
+player_score = 0
+computer_score = 0
 
 main_window.mainloop()
